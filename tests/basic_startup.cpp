03bc78045be8ae66fff15ef8882dbbdfe6b09980
igordosbruto-star <igordosbruto@gmail.com>
2025-08-22 17:30:37 -0300

    code error

--- tests/basic_startup.cpp
+++ tests/basic_startup.cpp
@@ -2,7 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 TEST(BasicStartup, CanCreateWindow) {
-    sf::RenderWindow window(sf::VideoMode(100, 100), "Test");
+    sf::RenderWindow window(sf::VideoMode({100, 100}), "Test");
     EXPECT_TRUE(window.isOpen());
     window.close();
 }
