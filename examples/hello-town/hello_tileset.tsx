ef16f9143820605050963e58a3e7c958307d2d84
igordosbruto-star <igordosbruto@gmail.com>
2025-08-24 00:02:52 -0300

    ty

---
+++ examples/hello-town/hello_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="hello_tileset" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../../../Downloads/Pixel background/32x32/3. Color/Picture/color_background_91.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../../../Downloads/Pixel background/32x32/3. Color/Picture/color_background_86.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../../../Downloads/Pixel background/32x32/3. Color/Picture/color_background_60.png" width="32" height="32"/>
+ </tile>
+</tileset>
---
+++ examples/hello-town/test_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="1" tileheight="1" tilecount="3072" columns="96">
+ <image source="tiles.png" width="96" height="32"/>
+</tileset>
