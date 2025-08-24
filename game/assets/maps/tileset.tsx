ff4c16ee6e479df22e1099e2dfc476a732fe72ef
igordosbruto-star <igordosbruto@gmail.com>
2025-08-24 14:26:32 -0300

    test(runtime): simulate player collision

--- game/assets/maps/tileset.tsx
+++ game/assets/maps/tileset.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="1" tileheight="1" tilecount="1" columns="1">
  <image source="tiles.png" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
